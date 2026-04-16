d66bfd9f716b59e495792bad0559f3b8ad979a90
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-16 10:05:15 +0400

    Task-1: StringOperationsDemo, was added and completed !

--- src/StringOperationsDemo.java
+++ src/StringOperationsDemo.java
@@ -1,2 +1,15 @@
 public class StringOperationsDemo {
+    public static void main(String[] args){
+        String customerName = "Aaisha AL-Rubaie";
+        String companyName = " - Oman Oil Marketing Company (OOMCO)";
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Length: " + customerName.length());
+        System.out.println("UpperCase: " + customerName.toUpperCase());
+        System.out.println("LowerCase: " + customerName.toLowerCase());
+
+       String combined = customerName.concat(companyName);
+        System.out.println("Combined String: " + combined);
+
+    }
 }
